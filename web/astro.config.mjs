ea80c7efdc545603bb54af9729c0eff6db9b6f27
Apolo Freitas <apolobrener11@gmail.com>
2024-08-05 21:53:34 -0400

    feat: add base url to work with github pages

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://athenacfr.github.io",
+  base: "athenacfr",
   redirects: {
-    "/": "/en",
+    "/": "/athenacfr/en",
   },
 });
