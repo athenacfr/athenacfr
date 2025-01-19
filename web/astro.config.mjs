c443b381004d39a153660a920fe14167baf24c40
Athena Freitas <athenacfr@gmail.com>
2025-01-19 02:02:40 -0800

    move curriculum to https://athenacfr.github.io/curriculum/

--- web/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://athenacfr.github.io",
-  base: "athenacfr",
-  redirects: {
-    "/": "/athenacfr/en",
-  },
-});
